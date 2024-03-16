9664d9e95e739d54371cf3e93272c2c98142791c
Indra Wijaya <indraakmal08@gmail.com>
2024-03-16 23:35:24 +0800

    Solve Round The Next Level

--- round-the-next-level.js
+++ round-the-next-level.js
@@ -1,3 +1,20 @@
 function roundToNext5(n) {
-  // ...
+  if (n % 5 === 0) {
+    return n;
+  } else {
+    if (n < 0) {
+      return n - (n % 5);
+    } else {
+      return n + (5 - (n % 5));
+    }
+  }
 }
+
+// console.log(roundToNext5(0));
+// console.log(roundToNext5(2));
+// console.log(roundToNext5(3));
+// console.log(roundToNext5(12));
+// console.log(roundToNext5(21));
+// console.log(roundToNext5(30));
+console.log(roundToNext5(-2));
+// console.log(roundToNext5(-5));
