e2ebc1e1d0e478a8b335d6fcf405aaf67ce08175
Indra Wijaya <indraakmal08@gmail.com>
2024-03-16 23:46:57 +0800

    Solve Sum A Squence

--- sum-a-sequence.js
+++ sum-a-sequence.js
@@ -1,3 +1,17 @@
 const sequenceSum = (begin, end, step) => {
   // May the Force be with you
+  if (start > end) {
+    return 0;
+  }
+
+  let sum = 0;
+  // Mulai dari nilai mulai, lalu tambahkan dengan langkah hingga mencapai atau melewati nilai akhir
+  for (let i = start; i <= end; i += step) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+console.log(sequenceSum(2, 2, 2));
+console.log(sequenceSum(2, 6, 2));
