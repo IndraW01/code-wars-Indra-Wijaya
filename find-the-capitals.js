049547f072b3aa152e28e72d4c1ae0c5e1795f53
Indra Wijaya <indraakmal08@gmail.com>
2024-03-19 23:38:28 +0800

    Solve Find The Capitals

--- find-the-capitals.js
+++ find-the-capitals.js
@@ -1,3 +1,14 @@
 var capitals = function (word) {
+  const result = [];
+
   // Write your code here
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == word[i].toUpperCase()) {
+      result.push(i);
+    }
+  }
+
+  return result;
 };
+
+console.log(capitals('CodEWaRs'));
