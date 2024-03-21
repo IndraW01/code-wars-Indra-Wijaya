b587450d3b3f3770e01f33522459f98f41770336
IndraW01 <indraakmal08@gmail.com>
2024-03-21 17:29:48 +0800

    Solve Check The Example

---
+++ check-the-examp.js
@@ -0,0 +1,20 @@
+function checkExam(array1, array2) {
+  let result = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array2[i] == '') {
+      continue;
+    }
+
+    if (array1[i] == array2[i]) {
+      result += 4;
+    }
+
+    if (array1[i] != array2[i]) {
+      result -= 1;
+    }
+  }
+
+  return result < 0 ? 0 : result;
+}
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))
