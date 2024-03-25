6e2400934e89a8d430f8c913c7dc13282e10bd4c
IndraW01 <indraakmal08@gmail.com>
2024-03-25 08:44:41 +0800

    Solve Maximum Multiple

--- maximum-multiple.js
+++ maximum-multiple.js
@@ -1,3 +1,9 @@
 function maxMultiple(divisor, bound) {
   //your code here
+  for (let i = bound - 1; i > 0; i--) {
+    if (i % divisor == 0) return i;
+  }
 }
+
+console.log(maxMultiple(2, 7));
+console.log(maxMultiple(3, 10));
