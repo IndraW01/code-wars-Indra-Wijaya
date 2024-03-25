6c757a41c3c0281bc9d09337b6137c75c370d731
IndraW01 <indraakmal08@gmail.com>
2024-03-25 08:51:32 +0800

    Solve Fix String Case

---
+++ fix-string-case.js
@@ -0,0 +1,19 @@
+function solve(s) {
+  let lower = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == s[i].toLowerCase()) {
+      lower += 1;
+    }
+  }
+
+  if (lower >= s.length / 2) {
+    return s.toLowerCase();
+  } else {
+    return s.toUpperCase();
+  }
+}
+
+console.log(solve('code'));
+console.log(solve('CODe'));
+console.log(solve('COde'));
