15d54098f2aedb1a0ed5dfaab575153eda2ad41a
IndraW01 <indraakmal08@gmail.com>
2024-05-06 12:39:21 +0800

    Solve Help The Fruit

---
+++ help-the-fruit.js
@@ -0,0 +1,14 @@
+function removeRotten(bagOfFruits) {
+  // ...
+  if (bagOfFruits.length < 1) return [];
+
+  return bagOfFruits.map(val => {
+    if (val.includes('rotten')) {
+      return val.split('rotten')[1].toLowerCase();
+    }
+
+    return val;
+  });
+}
+
+console.log(removeRotten(["apple", "rottenBanana", "kiwi", "melone", "orange"]));
